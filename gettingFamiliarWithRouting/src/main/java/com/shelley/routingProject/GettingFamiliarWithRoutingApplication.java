8c1e6f4fe4de3b73b0c35eb5091764bd8495862e
Shelley Ophir <qidcoder@gmail.com>
2020-12-07 16:09:55 -0500

    project initialization

---
+++ gettingFamiliarWithRouting/src/main/java/com/shelley/routingProject/GettingFamiliarWithRoutingApplication.java
@@ -0,0 +1,13 @@
+package com.shelley.routingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingFamiliarWithRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingFamiliarWithRoutingApplication.class, args);
+	}
+
+}
